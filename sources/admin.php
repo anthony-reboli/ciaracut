7c37f15fd93bd612c6e01e5945569fe1585b389e
anthony.reboli <anthony.reboli@laplateforme.io>
2020-06-04 14:29:17 +0200

    reservation faite

--- sources/admin.php
+++ sources/admin.php
@@ -10,7 +10,7 @@ $bdd = mysqli_connect("localhost", "root", "", "ciaracut");
 <head>
     <meta charset="UTF-8">
     <title>Espace Administrateur - ciaracut </title>
-    <link rel="stylesheet" href="boutique.css">
+    <link rel="stylesheet" href="../CSS/cira.css">
 </head>
 <body id="pageadmin">
 <header>
@@ -19,130 +19,18 @@ include("bar-nav.php");?>
 </header>
 <h1 class="titre">Nos Prestations</h1>
 		<section id="formadmin">
-						<div class="form">
-						<?php	
-							
-								
-								if (isset($_POST['valider'])) {
-
-									if (!empty($_POST['titre'])&& !empty($_POST['prix'])&& !empty($_POST['type'])) {
-													$titre = $_POST['titre'];
-									                $prix = $_POST['prix'];
-									                $description = $_POST['description'];
-									                $type = $_POST['type'];
-									                
-													$presta = "INSERT INTO prestation (nom,type,prix) VALUES ('$titre','$type', '$prix')";
-													$prestaQ=mysqli_query($bdd,$presta);
-													var_dump($presta);
-													
-												
-															//var_dump($connexion);
-									}
-								
-								}
-
-
-								if (isset($_POST['modifM'])) {
-
-									if (!empty($_POST['titreMP'])&& !empty($_POST['prixM'])&& !empty($_POST['typeM'])) {
-													$Tapres = $_POST['titreMP'];
-													$Tavant = $_POST['titreMA'];
-									                $prix = $_POST['prixM'];
-									                $description = $_POST['description'];
-									                $type = $_POST['typeM'];
-									                
-													$presta = "UPDATE prestation set nom='$Tapres',prix=$prix,type='$type' where nom='$Tavant'";
-													$prestaQ=mysqli_query($bdd,$presta);
-													var_dump($presta);
-													
-												
-															//var_dump($connexion);
-									}
-								
-								}
-								
-							  ?>
-							   <form method="post" >
-							  <button type="submit" name="creation">Prestations</button>
-							</form>
-							  <?php 
-							  	if(isset($_POST['creation']))
-							  	{
-							  		?>
-
-							  								<div id="createA">
-										<h3>Créer une prestation</h3>
-							  <form method="post" >
-							                    <label>titre</label></br>
-							                    <input type="text" name="titre"></br>							          
-							                    <label>Prix</label></br>
-							                    <input type="number" name="prix"></br>
-							                    <label>type</label></br>
-							                    <input type="text" name="type" required></br>
-							                    <input type="submit" value="Envoyer" name="valider"></br>
-							   </form>
-							   		</div>
-							   <div id="modifcre">
-							   	<h3>Modifier une prestation</h3>
-							   		<form method="post" >
-							                    <label>titre avant</label></br>
-							                    <input type="text" name="titreMA" required></br>
-							                    <label>titre apres</label></br>
-							                    <input type="text" name="titreMP" required></br>								          
-							                    <label>Prix</label></br>
-							                    <input type="number" name="prixM" required></br>
-							                    <label>type</label></br>
-							                    <input type="text" name="typeM" required ></br>
-							                    <input type="submit" value="Modifier" name="modifM"></br>
-							   		</form>
-							   </div>
-							</div>
-							<?php
-
-								}
-
-						?>
-
-						<!-- reservation -->
-							<form method="post" >
-							  	<button type="submit" name="reservation">Reservation</button>
-							</form>
-						<?php 
-						if(isset($_POST['reservation']))
-						{
-							$reserv="SELECT * FROM reservation inner join utilisateurs on id_utilisateurs = utilisateurs.id";
-							$reservQ=mysqli_query($bdd,$reserv);
-
+			<?php
+			include("../include/formadmin/formprestation.php");
+			?>
+						
+						
 
 						
 
-							while($data= mysqli_fetch_assoc($reservQ))
-						            {
-										 $i=0;
-						                $nom=$data['nom'];
-						                $prenom=$data['prenom'];
-						                $nais=$data['date_naissance'];
-						                $ids=$data['id'];
-						                $debut=$data['debut'];
-						                $fin=$data['fin'];
+						reservation
 
-						                var_dump($data);
-						                echo"<div id='infouser'>";
-						                echo "nom:&nbsp $nom &nbsp";
-						                echo "prenom:&nbsp $prenom &nbsp";
-						                echo "née: &nbsp$nais &nbsp";
-						                echo "debut:&nbsp$debut &nbsp";
-						                echo "debut:&nbsp$fin &nbsp";
-
-						                echo "<a href=\"profil.php?U=$ids\" target=\"_blank\"><button name=\"profil\">Profil</button></a>";
-						                echo"</div>";
-
-
-
-						                $i++;
-										
-						            }
-						}
+						<?php 
+			include("../include/formadmin/formreservation.php");
 
 
 						// Membre
---
+++ sources/reservation-form.php
@@ -0,0 +1,141 @@
+<?php session_start();
+unset($_SESSION['num']);
+
+
+#TA DATE EN TIME
+$datetime=time("Y-m-d H:i:s");
+
+#TU AJOUTES LE NOMBRE DE SECONDE DESIRE (ici 1h30 = 5400 secondes)
+$dateplus=$datetime+5400;
+
+#TU REPASSE EN FORMAT DATE
+$date=date("Y-m-d H:i:s",$dateplus);
+
+
+?>
+<html>
+<head>
+	<meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="camping.css">
+	<title>Réservation</title>
+</head>
+<body class="bodir">
+	<?php
+	include("bar-nav.php");
+    
+
+	if (isset($_SESSION['login']))
+	{
+        date_default_timezone_set('Europe/Paris');
+        $connexion = mysqli_connect("localhost","root","","ciaracut");
+        $requete ="SELECT * FROM utilisateurs WHERE login ='".$_SESSION['login']."'";
+        $query = mysqli_query($connexion, $requete);
+        $resultat = mysqli_fetch_all($query, MYSQLI_ASSOC);
+
+        $requete1 = "SELECT * FROM tarif2 ";
+
+        $req = mysqli_query($connexion, $requete1);
+
+
+	?>
+	<section  id="reservation-form">
+     	<h1 id="h1reservation-form"><b>Veuillez remplir le formulaire de la réservation</b></h1>
+     	<div class="parent">
+     	<article class="reservationf">
+
+     	                   <form class="form_site" method="post" action="reservation-form.php">
+                   <label for="text"><b>Titre</b></label>
+                   <input type="text" placeholder="Tapez le titre de l'Activite" name="titre" required>
+                   <label for="text"><b>Description</b></label>
+                   <input type="text" placeholder="Tapez une Description" name="description" required>
+                   <label for="datedebut"><b>Date debut</b></label>
+                   <input type="date" name="datedebut" required> 
+                   <label for="datefin"><b>Date fin</b></label>
+                   <input type="date" name="datefin" required> 
+                   <label for="heuredebut"><b>Heure debut</b></label>
+                   <input type="time" name="heuredebut" min="08:00" max="18:00" required> 
+                   <label for="heurefin"><b>Heure fin</b></label>
+                   <input type="time" name="heurefin" min="09:00" max="19:00" required> 
+                   
+                   <br>
+                   <input type="submit" value="SUBMIT EVENT" name="valider" />
+                   </form>
+     	</article>
+        <article class="comment">
+     	<?php
+        
+                    if ( isset($_POST["valider"]) )
+                    {
+                    	
+
+
+                          $titreresa = $_POST['titre'];
+                          $renametitre = addslashes($titreresa); 
+                          $descriptionresa = $_POST['description'];
+                          $renamedescritpion = addslashes($descriptionresa); 
+                          $datedebut = $_POST['datedebut']." ".$_POST['heuredebut'];
+                          $datefin = $_POST['datefin']." ".$_POST['heurefin'];
+                          $startdate = date('Y-m-d H:i:s', strtotime($datedebut));
+                          $enddate = date('Y-m-d H:i:s', strtotime($datefin));
+
+                        
+                    $entre="SELECT count(*) from reservation WHERE debut BETWEEN '$datedebut' AND '$datefin'";
+                    $queryentre=mysqli_query($connexion,$entre);
+                    $resultatentre=mysqli_fetch_all($queryentre);
+
+
+
+                          
+                        if($datedebut < date('Y-m-d'))
+                        {
+                              echo "Vous ne pouvez pas reserver a une date anterieur au".date('d-m-Y');
+                          
+                        }
+                        elseif ($datefin < $datedebut) 
+                        {
+                              echo "Vous ne pouvez pas choisir une date de fin antérieur a la date de debut";
+                        }
+                        else{
+                            
+                   				if($resultatentre[0][0] < 3)
+                    				{
+                                      
+
+                                      	$requete = "INSERT INTO reservation (titre, description, debut, fin) VALUES ('$renametitre', '$renamedescritpion', '$startdate', '$enddate')";
+                                      	
+                              			$query = mysqli_query($connexion, $requete);
+                              			
+                            		}
+                            		    else
+                    						{
+                    							echo "il y a deja 3 reservation pour c'est horaire";
+                    						}
+                                  
+                                  
+                    		}	
+                    }
+        
+               mysqli_close($connexion);
+        ?>
+        </article>
+    </div>
+     <?php
+   }
+   else 
+   {
+   ?>
+    <section id="notcon">
+      <p>Veuillez vous connecter pour accéder à votre page !</p>
+    </section>
+  <?php
+  }
+  include("footer.php");
+?>
+                         
+
+	
+
+
+
+</body>
+</html>
