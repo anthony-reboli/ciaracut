ea82e681ff528bb6ec147c1413caf5f5f679487c
anthony.reboli <anthony.reboli@laplateforme.io>
2020-06-05 15:40:11 +0200

    fonctionel

--- sources/reservation-form.php
+++ sources/reservation-form.php
@@ -1,3 +1,4 @@
+// reservation
 <?php session_start();
 unset($_SESSION['num']);
 
@@ -15,17 +16,17 @@ $date=date("Y-m-d H:i:s",$dateplus);
 ?>
 <html>
 <head>
-	<meta charset="utf-8">
+  <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="camping.css">
-	<title>Réservation</title>
+  <title>Réservation</title>
 </head>
 <body class="bodir">
-	<?php
-	include("bar-nav.php");
+  <?php
+  include("bar-nav.php");
     
 
-	if (isset($_SESSION['login']))
-	{
+  if (isset($_SESSION['login']))
+  {
         date_default_timezone_set('Europe/Paris');
         $connexion = mysqli_connect("localhost","root","","ciaracut");
         $requete ="SELECT * FROM utilisateurs WHERE login ='".$_SESSION['login']."'";
@@ -37,13 +38,13 @@ $date=date("Y-m-d H:i:s",$dateplus);
         $req = mysqli_query($connexion, $requete1);
 
 
-	?>
-	<section  id="reservation-form">
-     	<h1 id="h1reservation-form"><b>Veuillez remplir le formulaire de la réservation</b></h1>
-     	<div class="parent">
-     	<article class="reservationf">
+  ?>
+  <section  id="reservation-form">
+      <h1 id="h1reservation-form"><b>Veuillez remplir le formulaire de la réservation</b></h1>
+      <div class="parent">
+      <article class="reservationf">
 
-     	                   <form class="form_site" method="post" action="reservation-form.php">
+                         <form class="form_site" method="post" action="reservation-form.php">
                    <label for="text"><b>Titre</b></label>
                    <input type="text" placeholder="Tapez le titre de l'Activite" name="titre" required>
                    <label for="text"><b>Description</b></label>
@@ -60,13 +61,13 @@ $date=date("Y-m-d H:i:s",$dateplus);
                    <br>
                    <input type="submit" value="SUBMIT EVENT" name="valider" />
                    </form>
-     	</article>
+      </article>
         <article class="comment">
-     	<?php
+      <?php
         
                     if ( isset($_POST["valider"]) )
                     {
-                    	
+                      
 
 
                           $titreresa = $_POST['titre'];
@@ -97,22 +98,22 @@ $date=date("Y-m-d H:i:s",$dateplus);
                         }
                         else{
                             
-                   				if($resultatentre[0][0] < 3)
-                    				{
+                          if($resultatentre[0][0] < 3)
+                            {
                                       
 
-                                      	$requete = "INSERT INTO reservation (titre, description, debut, fin) VALUES ('$renametitre', '$renamedescritpion', '$startdate', '$enddate')";
-                                      	
-                              			$query = mysqli_query($connexion, $requete);
-                              			
-                            		}
-                            		    else
-                    						{
-                    							echo "il y a deja 3 reservation pour c'est horaire";
-                    						}
+                                        $requete = "INSERT INTO reservation (titre, description, debut, fin) VALUES ('$renametitre', '$renamedescritpion', '$startdate', '$enddate')";
+                                        
+                                    $query = mysqli_query($connexion, $requete);
+                                    
+                                }
+                                    else
+                                {
+                                  echo "il y a deja 3 reservation pour c'est horaire";
+                                }
                                   
                                   
-                    		}	
+                        } 
                     }
         
                mysqli_close($connexion);
@@ -133,7 +134,7 @@ $date=date("Y-m-d H:i:s",$dateplus);
 ?>
                          
 
-	
+  
 
 
 
