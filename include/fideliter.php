1a5d7f6199f72b406aedccd92c4d1c78d388d679
anthony.reboli <anthony.reboli@laplateforme.io>
2020-06-16 14:49:15 +0200

    fideliter a jour via une date d'aniv sur 7 jour

--- include/fideliter.php
+++ include/fideliter.php
@@ -1,47 +1,82 @@
-<form method="post">
-	<input type="submit" name="prix">
-</form>
+
 <?php
+$datejour=date('Y-m-d');
+
 $bdd = mysqli_connect("localhost", "root", "", "ciaracut");
+$selectotal="SELECT * FROM utilisateurs where nom ='aniv'";
+$selectotalQ=mysqli_query($bdd,$selectotal);
+$selectotalR=mysqli_fetch_all($selectotalQ);
+
+$dateR=$selectotalR[0][6];
+// $dateR = date('Y-m-d', $dateR);
+$dateRT=strtotime($dateR);
+		
+
+$test=strtotime("+7 day", $dateRT);
+$test=date('Y-m-d', $test);
+$datetime1 = date_create($dateR);
+$datetime2 = date_create($test);
+$datetime3 = date_create($datejour);
+$interval = date_diff($datetime1, $datetime2);
+$interval1 = date_diff($datetime1, $datetime3);
+
+
+				if($datetime3 >= $datetime1 == "true" and $datetime3 <= $datetime2 == "true")
+		{
+			?>
+			<form method="post">
+			<input type="submit" name="prix">
+			</form>
+			<?php
+		}
+		?>
+
+<?php
+
+
+
+
 
 	if(isset($_POST['prix']))
 	{
 
 
-	$dateanM=date('n');
-	$dateanJ=date('j');
 
-$selectS="SELECT id FROM utilisateurs where mois= '$dateanM' and jour = '$dateanJ'";
 
-$deux=mysqli_query($bdd,$selectS);
-$trois=mysqli_fetch_all($deux);
+		$selectS="SELECT * FROM sauvegarde where nom = 'aniv' and datepanier LIKE ('$datejour%')";
+		var_dump($selectS);
 
-	
-
-	$i=0;
-	 foreach ($trois as $key2) {
-
-	 	$idus=$key2[0];
-	 	
-
-	 	$i++;
-	 	$red="SELECT prixtotal from panier where id_utilisateurs= '$idus'";
-		$redQ=mysqli_query($bdd,$red);
-		$redR=mysqli_fetch_all($redQ);
+		$deux=mysqli_query($bdd,$selectS);
+		$trois=mysqli_fetch_all($deux);
 
 			
-				foreach ($redR as $key3) {
+
+			
+			 foreach ($trois as $key2) {
+			 	var_dump($key2);
+			 	$prix=$key2[4];
+			 	
+
+			 	
+			 	$red="SELECT prixtotal from sauvegarde where prixtotal= '$prix'";
+				$redQ=mysqli_query($bdd,$red);
+				$redR=mysqli_fetch_all($redQ);
+
 					
-											$a=$key3[0];
-											$b=10;
-											$resultat= $a - ($a * ($b/100));
+						foreach ($redR as $key3) {
+							var_dump($key3);
+													$a=$key3[0];
+													$b=10;
+													$resultat= $a - ($a * ($b/100));
+															
+													$prix="UPDATE sauvegarde set prixtotal= $resultat where nom = 'aniv' and datepanier LIKE  ('$datejour%')";
+													var_dump($prix);
 													
-											$prix="UPDATE panier set prixtotal= $resultat where id_utilisateurs = $idus";
-											$prixQ=mysqli_query($bdd,$prix);
-											
-											
-										 }
-	 							}
+													$prixQ=mysqli_query($bdd,$prix);
+													
+													
+												 }
+			 							}
 	 }
 
 
