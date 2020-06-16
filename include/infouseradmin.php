56bf2f9e0823acf9cfc821e91f4631ac019e6b65
anthony.reboli <anthony.reboli@laplateforme.io>
2020-06-16 16:48:57 +0200

    anniversaire plus infos

--- include/infouseradmin.php
+++ include/infouseradmin.php
@@ -2,8 +2,10 @@
  <?php
  $i=0;
  										
+ 										
 						                $nom=$data['nom'];
 						                $prenom=$data['prenom'];
+						                $date=$data['anniv'];
 						                
 						                $ids=$data['id'];
 
@@ -13,9 +15,9 @@
 						                echo "prenom:&nbsp $prenom &nbsp";
 						                 
 						                echo "<a href=\"profil.php?U=$ids\" target=\"_blank\"><button name=\"profil\">Profil</button></a>";
-						                	$dateanM=date('n');
-											$dateanJ=date('j');
-						                	if($data['jour']== $dateanJ and $data['mois']== $dateanM)
+										$datejour=date('Y-m-d');
+										
+						                	if($date == $datejour)
 						                	{
 						                		echo "<img src=\"../upload/gat.png\" style=\" height: 28px; width: 28px;\">";
 						                	}
