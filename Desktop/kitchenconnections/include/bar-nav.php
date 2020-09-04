291916bd08585e0eee7128450e1b1b999b872822
fauvel <fauvel411@gmail.com>
2020-09-04 12:42:24 +0200

    Inscription connexion

---
+++ Desktop/kitchenconnections/include/bar-nav.php
@@ -0,0 +1,84 @@
+
+  <?php
+    if (isset($_SESSION['login'])==false)
+    {
+    ?>
+  <ul class="nav justify-content-end  bg- text-white flex-sm-row">
+    <li class="nav-item"><a class="nav-link text-white active" href="index.php">Home</a></li>
+    <li class="nav-item"><a class="nav-link text-white active" href="../sources/tarif.php">Tarifs</a></li>
+    <li class="nav-item"><a class="nav-link text-white active" href="../sources/livreor.php">Livre d'or</a></li>
+    <li class="nav-item"><a class="nav-link text-white active" href="connexion.php">Connexion</a></li>
+    <li class="nav-item"><a class="nav-link text-white active" href="inscription.php">Inscription</a></li>
+  </ul>
+
+
+  
+     <?php
+    }
+     elseif(isset($_SESSION['login'])==true)
+
+    {
+       if($_SESSION['login'] =="vanessa")
+       {
+       
+    ?>
+    <ul id="menu-deroulant" class="nav justify-content-end  text-white flex-sm-row">
+      <li class="nav-item"><a class="nav-link text-white active" href="index.php">Home</a></li>
+       <li class="nav-item"><a class="nav-link text-white active" href="../sources/livreor.php">Livre d'or</a></li>
+      <li class="nav-item"><a class="nav-link text-white active" href="../sources/tarif.php">Tarifs</a></li>
+      <li class="nav-item"><a class="nav-link text-white active" href="../sources/profil.php">Profil</a></li>
+      <li class="nav-item"><a class="nav-link text-white active" href="index.php?deconnexion=true">Déconnexion</a></li>
+      <li class="list-group-item bg-dark"><a class="nav-link text-white" href="#">Administrateur</a>
+    <ul class="list-group">
+      <li class="list-group-item"><a class="nav-link active d justify-content-center" href="../sources/client.php">Mes clients</a><li>
+      <li class="list-group-item"><a class="nav-link active justify-content-center" href="../sources/planning.php">Mon Agenda</a></li>
+      <li class="list-group-item"><a class="nav-link active justify-content-center" href="../sources/boutiqueprestation.php">Boutique/ Paiement</a>
+      <li class="list-group-item"><a class="nav-link active justify-content-center " href="../sources/stock.php">Mon stock</a></li>
+    </ul>
+  </li>
+
+  </li>
+</ul>
+    
+
+    
+
+    
+     <?php
+                
+                if(isset($_GET['deconnexion']))
+                { 
+                   if($_GET['deconnexion']==true)
+                   {  
+                      session_unset();
+                      header("location:../sources/index.php");
+                   }
+                }
+     
+    }
+    else
+    {   
+    ?>
+      <ul class="nav justify-content-end  text-white flex-sm-row">
+        <li class="nav-item"><a class="nav-link text-white active" href="index.php">Home</a></li>
+        <li class="nav-item"><a class="nav-link text-white active" href="../sources/tarif.php">Tarifs</a></li>
+        <li class="nav-item"><a class="nav-link text-white active" href="../sources/profil.php">Profil</a></li>
+        <li class="nav-item"><a class="nav-link text-white active" href="../sources/livreor.php">Livre d'or</a></li>
+        <li class="nav-item"><a class="nav-link text-white active" href="index.php?deconnexion=true">Déconnexion</a></li>
+      </ul>
+     <?php
+                
+                if(isset($_GET['deconnexion']))
+                { 
+                   if($_GET['deconnexion']==true)
+                   {  
+                      session_unset();
+                      header("location:../sources/index.php");
+                   }
+                }
+    
+    }
+      
+  }
+             
+    ?>
---
+++ Desktop/kitchenconnections/include/footer.php
@@ -0,0 +1,70 @@
+
+ 	<!-- Footer -->
+<footer id="foot" class="page-footer font-small blue pt-4">
+
+  <!-- Footer Links -->
+  <div class="container-fluid text-center justify-content-space-around align-items-center">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-4 mt-md-0 mt-4 text-center ">
+
+        <!-- Content -->
+        <h5 class="text-uppercase text-center">Adresse</h5>
+        <p style="text-align: center;">Salon Mixte Ciaracut</p>
+        <p style="text-align: center;">20 boulevard de la concorde</p>
+        <p style="text-align: center;">13009 Marseille</p>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none pb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3 d-flex flex-column justify-content-space-around align-items-center">
+
+        <!-- Links -->
+        <h5 class="text-uppercase">Réseaux sociaux</h5>
+
+        <ul class="list-unstyled d-flex flex-row ">
+          <li class="pr-2">
+            <a href="#!"><img width="50" height="50" src="../upload/facebook.jpg"/></a>
+          </li>
+          <li class="pr-2">
+               <a href="#!"><img width="50" height="50" src="../upload/instagram-logo.jfif"/></a>
+          </li>
+         
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 class="text-uppercase">Horaires</h5>
+
+        <p>Ouverture de 9H15 à 18H.</p>
+        <p>Du lundi au samedi.Fermé le mercredi.</p>
+        <p>Tel: 04-86-97-24-89.</p>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">Ciara cut by Vanessa © 2020 Copyright
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+  
